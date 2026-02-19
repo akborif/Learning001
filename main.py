7baa3247ba59d5ee57172243cb3f219392fa24d2
akborif <fahrettin.akbori.tr@outlook.com>
2026-02-19 20:02:34 +0200

    crateing a python file

---
+++ main.py
@@ -0,0 +1,10 @@
+# implement a function that splits a string by spaces
+def split_string_by_spaces(inout_string):
+    return inout_string.split(' ')
+
+# test the function[[[]]]
+
+
+test_string = "Hello world this is a test"
+result = split_string_by_spaces(test_string)
+print(result)
